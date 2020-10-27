b3ba2a786a8eb61de51b75c12ecaca02aacc50c5
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2020-10-27 13:59:59 -0500

    Complete Shortener

--- server.js
+++ server.js
@@ -1,37 +1,69 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
 var cors = require('cors');
+var bodyParser = require('body-parser');
+const dns = require('dns');
+const url = require('url');
+
+var mongoose = require('mongoose');
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
 
 var app = express();
+app.use(cors({optionsSuccessStatus: 200}));
 
-// Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI, mongooseOptions);
+const ShortUrlSchema = mongoose.Schema({
+  url: {type: String, required: true},
+  short: Number,
+})
+const ShortUrl = mongoose.model("ShortUrl", ShortUrlSchema);
 
-app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(process.cwd() + '/public'));
+app.get('/', (req, res) => res.sendFile(process.cwd() + '/views/index.html'));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.post('/api/shorturl/new', (req, res) => {
+  let u;
+  try {
+    u = new url.URL(req.body.url)
+  } catch (err) {
+    res.json({error: 'invalid URL'})
+  }
+  dns.lookup(u.host, (err) => {
+    if (err) return res.json({error: 'invalid URL'});
+  });
+
+  if (u.protocol !== 'http:' && u.protocol !== 'https:')
+    return res.json({error: 'invalid URL'});
+
+  ShortUrl.find().sort({short: -1}).select({short: 1}).limit(1).exec((err, data) => {
+    if (err) return console.log(err);
+    const shortID = data[0].short + 1;
+
+    const Url = new ShortUrl({
+      url: req.body.url,
+      short: shortID,
+    })
+
+    Url.save((err, data) => {
+      if (err) return res.json({error: err});
+      res.json({original_url: req.body.url, short_url: shortID})
+    })
+  })
 });
 
+app.get('/api/shorturl/:shortID', (req, res) => {
+  ShortUrl.findOne({short: req.params.shortID}, (err, data) => {
+    if (err) return res.json({error: err});
+    if (!data) return res.json({error: 'url not found'})
+    res.redirect(data.url)
+  })
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function(req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-app.listen(port, function() {
-  console.log('Node.js listening on port ', port, ' ...');
-});
+app.listen(port, () => console.log('Node.js listening on port', port, '...'));
